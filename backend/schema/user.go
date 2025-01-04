e9c69d1169d332a2ad7137c5b62831e9aa23f9af
Shardendu Mishra <shardendumishrabrave@gmail.com>
2025-01-04 22:59:49 +0530

    Scehema Revamped

--- backend/schema/user.go
+++ backend/schema/user.go
@@ -8,27 +8,30 @@ type User struct {
 	Email     string    `json:"email"`
 	Password  string    `json:"password"`
 	Level     int       `json:"level"`
+	Bio       string    `json:"bio"`       // Keep bio concise
+	Followers []*User   `json:"followers"` // Use pointers to avoid circular references
+	Following []*User   `json:"following"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type Comment struct {
+	ID        uint      `json:"id"`
+	PostID    uint      `json:"post_id"`
+	UserID    uint      `json:"user_id"`
+	Content   string    `json:"content"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
 
 type Post struct {
-	ID       uint       `json:"id"`
-	UserID   uint       `json:"user_id"`
-	Title    string     `json:"title"`
-	Content  string     `json:"content"`
-	Likes    int        `json:"likes"`
-	Comments []Comment  `json:"comments"`
-	Image    string     `json:"image"`
-	CreatedAt time.Time  `json:"created_at"`
-	UpdatedAt time.Time  `json:"updated_at"`
-}
-
-type Comment struct {
-	ID       uint      `json:"id"`
-	PostID   uint      `json:"post_id"`
-	UserID   uint      `json:"user_id"`
-	Content  string    `json:"content"`
+	ID        uint      `json:"id"`
+	UserID    uint      `json:"user_id"`
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	Likes     int       `json:"likes"`
+	Comments  []Comment `json:"comments"`
+	Image     string    `json:"image"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
